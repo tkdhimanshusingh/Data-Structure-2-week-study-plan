059d256561e72e41495d641c4f9f02ebf917e74b
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-08-09 09:39:25 +0530

    Create 21. Merge Two Sorted Lists.py

---
+++ Day 7 Linked List/21. Merge Two Sorted Lists.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        temp=head=ListNode()
+        while list1 and list2:
+            if list1.val<list2.val:
+                temp.next=list1
+                list1=list1.next
+            else:
+                temp.next=list2
+                list2=list2.next
+            temp=temp.next
+        if list1:
+            temp.next=list1
+        if list2:
+            temp.next=list2
+        return head.next
